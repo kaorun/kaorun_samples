7fb0fd1e634a686c17599662e6b52e52ec686814
Kaoru Nakajima <kaorun@labs.cybozu.co.jp>
2015-05-12 17:42:43 +0900

    Original

---
+++ WpfScreenCaptureTest/WpfScreenCaptureTest/CaptureWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.Windows.Interop;
+using System.Windows.Threading;
+
+namespace kaorun.samples
+{
+    /// <summary>
+    /// マウスでドラッグして選択するウインドウクラス
+    /// </summary>
+    public partial class CaptureWindow : Window
+    {
+        public CaptureWindow()
+        {
+            InitializeComponent();
+            Cursor = Cursors.Cross;
+
+            var mouseDownEvents = Observable.FromEventPattern<MouseEventArgs>(this, "MouseLeftButtonDown");
+            var mouseMoveEvents = Observable.FromEventPattern<MouseEventArgs>(this, "MouseMove");
+            var mouseUpEvents = Observable.FromEventPattern<MouseEventArgs>(this, "MouseLeftButtonUp");
+
+            var startPos = new System.Windows.Point();
+            var drag = mouseDownEvents.SelectMany(down =>
+            {
+                startPos = down.EventArgs.GetPosition(LayoutRoot);
+                return mouseMoveEvents;
+            }).TakeUntil(mouseUpEvents).Select(e => BoundsRect(startPos.X, startPos.Y, e.EventArgs.GetPosition(LayoutRoot).X, e.EventArgs.GetPosition(LayoutRoot).Y));
+            drag.Subscribe(e =>
+            {
+                Canvas.SetLeft(selectionRect, e.X);
+                Canvas.SetTop(selectionRect, e.Y);
+                selectionRect.Width = e.Width;
+                selectionRect.Height = e.Height;
+            });
+            mouseUpEvents.Subscribe(e =>
+            {
+                this.Hide();
+                // WPFのウインドウを枠無しで最大化すると微妙にオフセットするので補正している。
+                var bmp = CaptureScreen(Rect.Offset(BoundsRect(startPos.X, startPos.Y, e.EventArgs.GetPosition(LayoutRoot).X, e.EventArgs.GetPosition(LayoutRoot).Y), this.Left, this.Top));
+                MainWindow.Captured = bmp;
+                Cursor = Cursors.Arrow;
+                this.Close();
+            });
+        }
+
+        private static Rect BoundsRect(double left, double top, double right, double bottom)
+        {
+            return new Rect(Math.Min(left, right), Math.Min(top, bottom), Math.Abs(right - left), Math.Abs(bottom - top));
+        }
+
+        public static BitmapSource CaptureScreen(Rect rect)
+        {
+            using (var screenBmp = new Bitmap((int)rect.Width, (int)rect.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+            {
+                using (var bmpGraphics = Graphics.FromImage(screenBmp))
+                {
+                    bmpGraphics.CopyFromScreen((int)rect.X, (int)rect.Y, 0, 0, screenBmp.Size);
+                    return Imaging.CreateBitmapSourceFromHBitmap(
+                        screenBmp.GetHbitmap(),
+                        IntPtr.Zero,
+                        Int32Rect.Empty,
+                        BitmapSizeOptions.FromEmptyOptions());
+                }
+            }
+        }
+    }
+}
---
+++ WpfScreenCaptureTest/WpfScreenCaptureTest/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace kaorun.samples
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static MainWindow instance;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            instance = this;
+            this.DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var wnd = new CaptureWindow();
+            this.Visibility = Visibility.Hidden;
+            wnd.ShowDialog();
+            this.Visibility = Visibility.Visible;
+        }
+
+        public static BitmapSource Captured
+        {
+            get { return (BitmapSource)instance.GetValue(CapturedProperty); }
+            set { instance.SetValue(CapturedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Captured.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CapturedProperty =
+            DependencyProperty.Register("Captured", typeof(BitmapSource), typeof(MainWindow), new PropertyMetadata(null));
+
+    }
+}
